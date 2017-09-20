a3f1de9fc348f4c32fddea118c608b9b8c051a46
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air-5.local>
2017-09-19 23:22:16 -0700

    add to origin

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'welcome/index'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
